c010751860b28bdb05ead763ee3d5a590ffeeee3
Omer Dotchamou <omer.dotchamou@scalens.com>
2026-01-07 12:11:40 +0100

    Add database and swagger configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api', { exclude: ['api'] });
+
+  // Swagger Setup
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('Documentation backend API')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header',
+    })
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
